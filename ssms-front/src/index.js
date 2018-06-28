61de1f8af477783df2adc25cc2b9dba040f3a780
ahmady016 <ahmady09@gmail.com>
2018-06-28 17:20:08 +0200

    delete unwanted files from front-end

--- ssms-front/src/index.js
+++ ssms-front/src/index.js
@@ -1,9 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
-import M from 'materialize-css'; 
+import M from 'materialize-css';
 
 M.AutoInit();
 
