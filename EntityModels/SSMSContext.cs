bc391bb35afa581116bd4f5a3c01d82f23ff0a9e
ahmady016 <ahmady09@gmail.com>
2018-07-01 01:34:28 +0200

    adding dot in SQL Server conn string

--- EntityModels/SSMSContext.cs
+++ EntityModels/SSMSContext.cs
@@ -54,7 +54,7 @@ namespace SSMS.EntityModels
             if (!optionsBuilder.IsConfigured)
             {
                 //#warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
-                optionsBuilder.UseSqlServer(@"Server=DESKTOP-8GK916E;Database=ssms;Trusted_Connection=True;");
+                optionsBuilder.UseSqlServer(@"Server=.;Database=ssms;Trusted_Connection=True;");
             }
         }
 
