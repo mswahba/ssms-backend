40a04641f7f8a9a2b9bcd6c0ddab613e74022566
ahmady016 <ahmady09@gmail.com>
2018-07-29 01:27:40 +0200

    init Schools crud component and EduAssetsState component

--- ssms-front/src/App.js
+++ ssms-front/src/App.js
@@ -4,28 +4,30 @@ import UsersList from './users/UsersList';
 import SignUpParent from './users/SignUpParent';
 import Hijri from './shared/Hijri';
 import HGDatePicker from './shared/HGDatePicker';
+import Schools from './basic/Schools'
 import './App.css';
 
 class App extends Component {
-  logDates = (dates) => console.log(dates);
+  // logDates = (dates) => console.log(dates);
   render() {
     return (
       <Fragment>
+        <Schools />
       {/*
+        <div className="my-container">
+          <HGDatePicker label="HireDate"
+                        HKey="HireDateH"
+                        GKey="HireDateG"
+                        HDate={null}
+                        GDate={new Date()}
+                        getDates={this.logDates}
+          />
+        </div>
         <SignIn/>
         <UsersList/>
         <SignUpParent/>
         <Hijri />
       */}
-      <div className="container">
-        <HGDatePicker label="HireDate"
-                      HKey="HireDateH"
-                      GKey="HireDateG"
-                      HDate={null}
-                      GDate={new Date()}
-                      getDates={this.logDates}
-        />
-      </div>
       </Fragment>
     );
   }
---
+++ ssms-front/src/basic/Schools.js
@@ -0,0 +1,116 @@
+import React, { Component } from "react";
+import M from "materialize-css";
+import EduAssetsState from "./EduAssetsState";
+
+export default class Schools extends Component {
+  state = {};
+
+  initDatePicker = () => {
+    document.addEventListener("DOMContentLoaded", function() {
+      var elems = document.querySelector(".datepicker");
+      M.Datepicker.init(elems, { format: "dd/mm/yyyy" });
+    });
+  };
+
+  componentDidMount() {
+    this.initDatePicker();
+  }
+
+  render() {
+    return (
+      <EduAssetsState keys={["schools"]}>
+        {({ schools }) => (
+          <form>
+            <section className="section container">
+              <h3 className="orange-text">Add New School</h3>
+              <div className="divider orange" />
+              <div className="input-field ">
+                <i className="material-icons prefix">edit</i>
+                <input id="schoolId" type="text" className="validate" />
+                <label htmlFor="schoolId">School Number</label>
+              </div>
+              <div className="input-field">
+                <i className="material-icons prefix">edit</i>
+                <input id="schoolName" type="text" className="validate" />
+                <label htmlFor="schoolName">School Name [Arabic] </label>
+              </div>
+              <div className="input-field">
+                <i className="material-icons prefix">edit</i>
+                <input id="schoolNameEn" type="text" className="validate" />
+                <label htmlFor="schoolNameEn">School Name [English] </label>
+              </div>
+              <div className="input-field">
+                <i className="material-icons prefix">date_range</i>
+                <input id="startDate" type="text" className="datepicker" />
+                <label htmlFor="startDate">Start Date </label>
+              </div>
+              <div className="input-field">
+                <i className="material-icons prefix">home</i>
+                <textarea
+                  id="address"
+                  type="text"
+                  className="materialize-textarea validate"
+                />
+                <label htmlFor="address">Address </label>
+              </div>
+              <div className="input-field switch">
+                <i className="material-icons prefix">school</i>
+                <div id="isActive">
+                  <label>
+                    Activate School? No
+                    <input type="checkbox" />
+                    <span class="lever" />
+                    Yes
+                  </label>
+                </div>
+              </div>
+              <div className="input-field">
+                <i className="material-icons prefix">home</i>
+                <input
+                  id="upload"
+                  type="file"
+                  className="materialize-textarea validate"
+                />
+                <label htmlFor="address">Address </label>
+              </div>
+              <div className="input-field center">
+                <button
+                  class="btn waves-effect waves-light orange darken-3"
+                  type="submit"
+                  name="action"
+                >
+                  Save New School
+                  <i class="material-icons right">send</i>
+                </button>
+              </div>
+            </section>
+            <section className="section">
+              <table className="container responsive-table striped highlight">
+                <thead className="blue lighten-1">
+                  <tr>
+                    <th className="center">ID</th>
+                    <th className="center">Name</th>
+                    <th className="center">Start Date</th>
+                    <th className="center">Address</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {schools &&
+                    schools.map(school => (
+                      <tr>
+                        <td>{school.schoolId}</td>
+                        <td>{school.schoolName}</td>
+                        <td>{school.startDate}</td>
+                        <td>{school.address}</td>
+                        <td>{school.isActive}</td>
+                      </tr>
+                    ))}
+                </tbody>
+              </table>
+            </section>
+          </form>
+        )}
+      </EduAssetsState>
+    );
+  }
+}
