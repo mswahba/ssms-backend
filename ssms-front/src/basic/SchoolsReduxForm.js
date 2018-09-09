b7abf2d14633221a465db6ba2bfe530929acc930
ahmady016 <ahmady09@gmail.com>
2018-09-09 22:40:22 +0200

    Refactor SchoolsReduxForm adding validation, renderSwitch FormInput

--- ssms-front/src/basic/SchoolsReduxForm.js
+++ ssms-front/src/basic/SchoolsReduxForm.js
@@ -2,7 +2,13 @@ import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import { getTranslate } from 'react-localize-redux'
 import { Field, reduxForm } from 'redux-form'
-import { renderInput, renderDatepicker } from '../shared/FormInputs'
+import isAlpha from 'validator/lib/isAlpha'
+import isLength from 'validator/lib/isLength'
+import isBefore from 'validator/lib/isBefore'
+import isNumeric from 'validator/lib/isNumeric'
+import moment from 'moment'
+import { store } from '../AppStore'
+import { renderInput, renderDatepicker, renderSwitch } from '../shared/FormInputs'
 import { lookupActions } from '../store/lookup'
 import { initDatePicker, formatDate } from '../helpers'
 
@@ -57,7 +63,7 @@ class _SchoolsReduxForm extends Component {
   render() {
     const { handleSubmit, trans } = this.props;
     return (
-      <form className="rtl" onSubmit={handleSubmit}>
+      <form className="rtl" onSubmit={handleSubmit(console.log)}>
         <div>
           <Field name="schoolId"
                 label={trans("schools.fields.schoolId")}
@@ -82,6 +88,7 @@ class _SchoolsReduxForm extends Component {
         </div>
         <div>
           <Field name="address"
+                type="textarea"
                 label={trans("schools.fields.address")}
                 component={renderInput} />
         </div>
@@ -93,7 +100,7 @@ class _SchoolsReduxForm extends Component {
         <div>
           <Field name="isActive"
                 label={trans("schools.fields.isActive")}
-                component={renderInput} />
+                component={renderSwitch} />
         </div>
         <button type="submit">Submit</button>
       </form>
@@ -101,10 +108,39 @@ class _SchoolsReduxForm extends Component {
   }
 }
 
+const validate = ({ schoolName, schoolNameEn, startDate, address, comNum }) => {
+  const trans = getTranslate(store.getState().localize);
+  const errors = {};
+  // schoolName
+  if (!schoolName)
+    errors.schoolName = trans("schools.validations.schoolNameAr.required");
+  else if (!isAlpha(schoolName, 'ar'))
+    errors.schoolName = trans("schools.validations.schoolNameAr.alpha");
+  // schoolNameEn
+  if (!schoolNameEn)
+    errors.schoolNameEn = trans("schools.validations.schoolNameEn.required");
+  else if (!isAlpha(schoolNameEn))
+    errors.schoolNameEn = trans("schools.validations.schoolNameEn.alpha");
+  // startDate
+  if( startDate && !isBefore(moment(startDate).format('MM/DD/YYYY'), moment().add(1, 'day').format('MM/DD/YYYY') ) )
+    errors.startDate = trans("schools.validations.startDate.before");
+  // address
+  if(address && !isLength(address, { min:0, max: 250}))
+    errors.address = trans("schools.validations.address.length");
+  // comNum
+  if(comNum) {
+    if(!isNumeric(comNum, { no_symbols: true }))
+      errors.comNum = trans("schools.validations.comNum.numeric");
+    else if( !isLength(comNum, { min: 0, max: 6 }) )
+      errors.comNum = trans("schools.validations.comNum.length");
+  }
+  return errors;
+}
 // define the redux form
 const SchoolsReduxForm = reduxForm({
   form: 'schools',
-  enableReinitialize: true
+  enableReinitialize: true,
+  validate
 })(_SchoolsReduxForm);
 // select the values needed form redux state
 const mapStateToProps = (state) => ({
--- ssms-front/src/shared/FormInputs.js
+++ ssms-front/src/shared/FormInputs.js
@@ -1,37 +1,56 @@
 import React from 'react'
+
+const showError = ( { error, touched } ) => (
+  (touched && error)
+  ? <div className="red-text text-darken-4 valign-wrapper">
+      <i class="material-icons">error_outline</i>
+      {error}
+    </div>
+  : null
+)
+
 // form Inputs
-export const renderInput = (field) => {
+export const renderInput = ({ input, icon, type, label, meta }) => {
   return (
     <div className="input-field">
-      <i className="material-icons prefix">{field.icon || 'edit'}</i>
-      <input id={field.input.name} type={field.type || 'text'} {...field.input} />
-      <label className={field.input.value? 'active': ''} htmlFor={field.input.name}>{ field.label }</label>
-      {
-        (field.meta.touched && field.meta.error)
-        ? <div className="red-text text-darken-4 valign-wrapper">
-            <i class="material-icons">error_outline</i>
-            {field.error}
-          </div>
-        : null
+      <i className="material-icons prefix">{icon || 'edit'}</i>
+      { (type === 'textarea')
+          ? <textarea id={input.name} {...input} className="materialize-textarea" />
+          : <input id={input.name} type={type || 'text'} {...input} />
       }
+      <label className={input.value? 'active': ''} htmlFor={input.name}>{ label }</label>
+      { showError(meta) }
     </div>
   )
 }
 
-export const renderDatepicker = (field) => {
+export const renderDatepicker = ({ input, icon, type, label, meta }) => {
   return (
     <div className="input-field">
-      <i className="material-icons prefix">{field.icon || 'edit'}</i>
-      <input id={field.input.name} type='text' className={field.type} {...field.input} />
-      <label className={field.input.value? 'active': ''} htmlFor={field.input.name}>{ field.label }</label>
-      {
-        (field.meta.touched && field.meta.error)
-        ? <div className="red-text text-darken-4 valign-wrapper">
-            <i class="material-icons">error_outline</i>
-            {field.error}
-          </div>
-        : null
-      }
+      <i className="material-icons prefix">{icon || 'edit'}</i>
+      <input id={input.name} type='text' className={type} {...input} />
+      <label className={input.value? 'active': ''} htmlFor={input.name}>{ label }</label>
+      { showError(meta) }
     </div>
   )
 }
+
+export const renderSwitch = ({ input, meta, icon, label, on, off, hidden, disabled }) => (
+  <div className="input-field switch" hidden={hidden}>
+    <i className="material-icons prefix">{icon}</i>
+    <div>
+      <label className={input.value? 'active': ''} >
+        <span>{label}</span>
+        {off && <span>{off}</span>}
+        <input id="isActive"
+              disabled={disabled}
+              type="checkbox"
+              {...input}
+        />
+        <span className="lever" />
+        {on && <span>{on}</span>}
+      </label>
+    </div>
+    { showError(meta) }
+  </div>
+)
