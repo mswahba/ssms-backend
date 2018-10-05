c661324b696ae51fcbd5e61b679cad208d328d18
ahmady016 <ahmady09@gmail.com>
2018-10-05 13:39:16 +0200

    continue refactoring SignUpParent add react-select, fix renderCheck

--- ssms-front/src/shared/FormInputs.js
+++ ssms-front/src/shared/FormInputs.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import HGDatePicker from '../shared/HGDatePicker'
-import { AutoComplete } from 'primereact/autocomplete'
+import Select from 'react-select'
+import { change, touch } from 'redux-form'
+import { store } from '../AppStore'
 
 // #region show validations messages
 const showError = ( { error, touched } ) => (
@@ -16,158 +18,252 @@ const showError = ( { error, touched } ) => (
 // #region Action Button
 export const Button = ({ name, hidden, disabled, classes, icon, label }) => {
   let btnClasses = `btn waves-effect waves-light ${classes} `;
-  if(hidden)
-    btnClasses += 'hidden';
+  if (hidden) btnClasses += "hidden";
   return (
-    <button id={name}
-            name={name}
-            className={btnClasses}
-            disabled={disabled}
-    >
-      <i className="material-icons left">{ icon || 'send' }</i>
-      { label }
+    <button id={name} name={name} className={btnClasses} disabled={disabled}>
+      <i className="material-icons left">{icon || "send"}</i>
+      {label}
     </button>
-  )
-}
+  );
+};
 // #endregion
 
 // #region Inputs and Textarea
-export const renderInput = ({ input, icon, type, label, meta, uiState: {hidden, disabled} = {} }) => {
-  return (
+export const renderInput = ({
+  input,
+  icon,
+  type,
+  label,
+  meta,
+  uiState: { hidden, disabled } = {}
+}) => (
     <div className="input-field" hidden={hidden}>
-      <i className="material-icons prefix">{icon || 'edit'}</i>
-      { (type === 'textarea')
-          ? <textarea
-              id={input.name}
-              className="materialize-textarea"
-              disabled={disabled}
-              {...input}
-            />
-          : <input
-              id={input.name}
-              type={type === 'autocomplete'? 'text' : type}
-              className={type === 'autocomplete'? 'autocomplete' : ''}
-              disabled={disabled}
-              {...input}
-            />
+      <i className="material-icons prefix">{icon || "edit"}</i>
+      { type === "textarea"
+        ? <textarea
+            id={input.name}
+            className="materialize-textarea"
+            disabled={disabled}
+            {...input}
+          />
+        : <input
+            id={input.name}
+            type={type === "autocomplete" ? "text" : type}
+            className={type === "autocomplete" ? "autocomplete" : ""}
+            disabled={disabled}
+            {...input}
+          />
       }
-      <label className={input.value? 'active': ''} htmlFor={input.name}>{ label }</label>
-      { showError(meta) }
+      <label className={input.value ? "active" : ""} htmlFor={input.name}>{label}</label>
+      {showError(meta)}
     </div>
-  )
-}
+  );
 // #endregion
 
-// #region date and time pickers
-export const renderDatepicker = ({ input, icon, type, label, meta, uiState: {hidden, disabled} = {} }) => {
-  return (
+// #region materialize [date - time] pickers
+export const renderDatepicker = ({
+  input,
+  icon,
+  type,
+  label,
+  meta,
+  uiState: { hidden, disabled } = {}
+}) => (
     <div className="input-field" hidden={hidden}>
-      <i className="material-icons prefix">{icon || 'edit'}</i>
-      <input id={input.name} type='text' className={type} disabled={disabled} {...input} />
-      <label className={input.value? 'active': ''} htmlFor={input.name}>{ label }</label>
-      { showError(meta) }
+      <i className="material-icons prefix">{icon || "edit"}</i>
+      <input
+        id={input.name}
+        type="text"
+        className={type}
+        disabled={disabled}
+        {...input}
+      />
+      <label className={input.value ? "active" : ""} htmlFor={input.name}>
+        {label}
+      </label>
+      {showError(meta)}
     </div>
-  )
-}
+);
 // #endregion
 
 // #region Switch
-export const renderSwitch = ({ input, meta, icon, label, on, off, uiState: {hidden, disabled} = {} }) => (
+export const renderSwitch = ({
+  input,
+  meta,
+  icon,
+  label,
+  on,
+  off,
+  uiState: { hidden, disabled } = {}
+}) => (
   <div className="input-field" hidden={hidden}>
-    <i className="material-icons prefix">{icon || 'edit'}</i>
+    <i className="material-icons prefix">{icon || "edit"}</i>
     <label className="active">{label}</label>
     <div className="switch">
       <label>
         {off && <span>{off}</span>}
-        <input id="isActive"
-              type="checkbox"
-              disabled={disabled}
-              checked={input.value}
-              {...input}
+        <input
+          id="isActive"
+          type="checkbox"
+          disabled={disabled}
+          checked={input.value}
+          {...input}
         />
         <span className="lever" />
         {on && <span>{on}</span>}
       </label>
     </div>
-    { showError(meta) }
+    {showError(meta)}
   </div>
-)
+);
 // #endregion
 
 // #region Select [Dropdown list]
-export const renderSelect = ({ meta, input, label, options, icon = 'input', uiState: {hidden, disabled} = {} }) => (
+export const renderSelect = ({
+  meta,
+  input,
+  label,
+  options,
+  icon = "input",
+  uiState: { hidden, disabled } = {}
+}) => (
   <div className="input-field" hidden={hidden}>
-  <i className="material-icons prefix">{icon}</i>
-  <select id={input.name}
-          disabled={disabled}
-          {...input}
-  >
-    <option value=''>{label || input.name}</option>
-    {options.map(opt => <option key={opt.value} value={opt.value}>{opt.text}</option>)}
-  </select>
-  <label htmlFor={input.name}>{label || input.name}</label>
-  { showError(meta) }
-</div>
-)
+    <i className="material-icons prefix">{icon}</i>
+    <select id={input.name} disabled={disabled} {...input}>
+      <option value="">{label || input.name}</option>
+      {options.map(opt => (
+        <option key={opt.value} value={opt.value}>{opt.text}</option>
+      ))}
+    </select>
+    <label htmlFor={input.name}>{label || input.name}</label>
+    {showError(meta)}
+  </div>
+);
 // #endregion
 
 // #region Radios and Checkboxes
-export const renderCheck = ({ meta, input, label, options, type = 'radio', icon = 'edit', labelClassName= '', itemsClassName = '', itemClassName = '', inputClassName= '', uiState: {hidden, disabled} = {} }) => (
-  <div className="input-field" hidden={hidden}>
-    <i className="material-icons prefix">{icon}</i>
-    <label className={labelClassName} htmlFor={input.name}>{ label || input.name}</label>
-    <div className={itemsClassName}>
-      {options.map(opt => (
-        <label key={opt.value} htmlFor={opt.value} className={itemClassName}>
-          <input type={type}
-                  id={opt.value}
-                  className={inputClassName}
-                  disabled={disabled}
-                  {...input}
-        />
-          <span>{opt.text}</span>
-        </label>
-      ))}
+export const renderCheck = ({
+  meta,
+  input,
+  label,
+  options,
+  formName,
+  type = "radio",
+  icon = "edit",
+  labelClassName = "",
+  itemsClassName = "",
+  itemClassName = "",
+  inputClassName = "",
+  uiState: { hidden, disabled } = {}
+}) => (
+    <div className="input-field" hidden={hidden}>
+      <i className="material-icons prefix">{icon}</i>
+      <label className={labelClassName} htmlFor={input.name}>
+        {label || input.name}
+      </label>
+      <div className={itemsClassName}>
+        {options.map(opt => (
+          <label key={opt.value} htmlFor={opt.value} className={itemClassName}>
+            <input
+              type={type}
+              name={input.name}
+              id={opt.value}
+              className={inputClassName}
+              disabled={disabled}
+              value={
+                (type === 'radio')
+                  ? opt.value
+                  : input.value.includes(opt.value)
+              }
+              onChange={ ({ target }) => {
+                if(type === 'radio')
+                  store.dispatch(change(formName, input.name, target.value))
+                else {
+                  const values = target.checked
+                    ? [...input.value, opt.value]
+                    : input.value.filter(val => val !== opt.value)
+                  store.dispatch(change(formName, input.name, values ))
+                }
+              } }
+              onBlur={ () => store.dispatch(touch(formName, input.name)) }
+              onFocus={input.onFocus}
+            />
+            <span>{opt.text}</span>
+          </label>
+        ))}
+      </div>
+      {showError(meta)}
+      <pre style={ { direction: "ltr", textAlign: "left" } }>
+        <code>{`touched: ${meta.touched}`}</code>
+      </pre>
     </div>
-    { showError(meta) }
-  </div>
-)
+  );
 // #endregion
 
 // #region HGDatepicker
-export const renderHGDatepicker = ({ meta, input, label, HLabel, GLabel, HKey, GKey, icon = 'edit', HDate = null, GDate = new Date(), onSelect, uiState: {hidden, disabled} = {} }) => (
+export const renderHGDatepicker = ({
+  meta,
+  input,
+  label,
+  HLabel,
+  GLabel,
+  HKey,
+  GKey,
+  icon = "edit",
+  HDate = null,
+  GDate = new Date(),
+  onSelect,
+  uiState: { hidden, disabled } = {}
+}) => (
   <div className="input-field" hidden={hidden}>
     <i className="material-icons prefix hg-icon">{icon}</i>
     <HGDatePicker
-        label={label}
-        HLabel={HLabel}
-        GLabel={GLabel}
-        HKey={HKey}
-        GKey={GKey}
-        HDate={HDate}
-        GDate={GDate}
-        getDates={onSelect}
-        {...input}
+      label={label}
+      HLabel={HLabel}
+      GLabel={GLabel}
+      HKey={HKey}
+      GKey={GKey}
+      HDate={HDate}
+      GDate={GDate}
+      getDates={onSelect}
+      {...input}
     />
-    { showError(meta) }
+    {showError(meta)}
   </div>
-)
+);
 // #endregion
 
-// #region primereact AutoComplete
-export const renderAutoComplete = ({ meta, input, label, onChange, completeMethod, options = {}, icon = 'edit', uiState: {hidden, disabled} = {} }) => (
+// #region React Select [Select - Autocomplete [single-multi] ]
+export const renderAutoComplete = ({
+  meta,
+  input,
+  label,
+  formName,
+  defaultValue = null,
+  options = {},
+  settings = {},
+  icon = "edit",
+  className = "validate",
+  uiState: { hidden, disabled } = {}
+}) => (
   <div className="input-field" hidden={hidden}>
     <i className="material-icons prefix">{icon}</i>
-    <AutoComplete
-        className="validate"
-        label={label}
-        disabled={disabled}
-        completeMethod={completeMethod}
-        {...options}
-        {...input}
-        onChange={onChange}
+    <Select
+      className={className}
+      isDisabled={disabled}
+      placeholder={label}
+      defaultValue={defaultValue}
+      options={options}
+      name={input.name}
+      onChange={ selectedOption => store.dispatch(change(formName, input.name, selectedOption && selectedOption.value)) }
+      onBlur={ () => store.dispatch(touch(formName, input.name)) }
+      onFocus={input.onFocus}
+      {...settings}
     />
-    { showError(meta) }
+    {showError(meta)}
+    <pre style={ { direction: "ltr", textAlign: "left" } }>
+        <code>{`touched: ${meta.touched}`}</code>
+    </pre>
   </div>
-)
+);
 // #endregion
--- ssms-front/src/users/SignUpParent.js
+++ ssms-front/src/users/SignUpParent.js
@@ -8,14 +8,32 @@ import { userActions } from '../store/user'
 import { store } from '../AppStore'
 import { initAutoComplete } from '../helpers'
 
-class SignUpParent extends Component {
+// #region local variables
+const initialValues = {
+    fName: '',
+    mName: '',
+    gName: '',
+    lName: '',
+    countryId: null,
+    idType: '',
+    parentId: '',
+    idExpireDate: {idExpireDateG: '', idExpireDateH: ''},
+    mobile: '',
+    email: '',
+    userId: '',
+    password: '',
+}
+const formName = "signUpParent";
+// #endregion
 
+// #region The SignUpParent Form Component
+class SignUpParent extends Component {
+  // init state - get lookup data [docTypes, countries]
   constructor(props) {
     super(props);
     this.state = {
       idTypeOptions: [],
-      countriesOptions: [], // {}
-      countriesSuggestions: []
+      countriesOptions: []
     }
     this.initAutoComplete = true;
     lookupActions.setSelectedTables(['docTypes','countries']);
@@ -28,179 +46,137 @@ class SignUpParent extends Component {
     }
     );
   }
-
+  // set displayField of [docTypes, countries] based on activeLanguage and
+  // map each array of [docTypes, countries] from string to object with [text/label - value]
   componentWillReceiveProps(nextProps) {
-    const { language } = nextProps;
-    const docTypeField = language == 'en'? 'docTypeEn' : 'docTypeAr';
-    const countryField = language == 'en'? 'countryEn' : 'countryAr';
+    const { trans } = nextProps;
     if (nextProps.docTypes.length && nextProps.countries.length) {
       this.setState({
         idTypeOptions: nextProps.docTypes.map(item => ({
-          text: item[docTypeField],
+          text: item[trans("users.signUpParent.docTypeField")],
           value: item.docTypeId
         })),
-        countriesOptions: nextProps.countries.map(country => ({ countryId: country.countryId, [countryField]: country[countryField] }) )
-        // countriesOptions: nextProps.countries.reduce((data, item) => {
-        //   data[item[countryField]] = item.countryId;
-        //   return data;
-        // }, {})
+        countriesOptions: nextProps.countries.map(country => ({
+          label: country[trans("users.signUpParent.countryField")],
+          value: country.countryId
+        }))
       })
     }
   }
-
+  // manually set idExpireDate field values [idExpireDateG - idExpireDateH]
   setDates = (values) => {
-    const { dispatch } = this.props;
-    dispatch(change('signUpParent', 'idExpireDate', values ));
+    this.props.dispatch(change('signUpParent', 'idExpireDate', values ));
   }
-
-  doAutoComplete = ({ query }) => {
-    console.log('TCL: ---------------------------------------------------');
-    console.log('TCL: SignUpParent -> doAutoComplete -> query', query);
-    console.log('TCL: ---------------------------------------------------');
-    const { countriesOptions } = this.state;
-    console.log('TCL: -------------------------------------------------------------------------');
-    console.log('TCL: SignUpParent -> doAutoComplete -> countriesOptions', countriesOptions);
-    console.log('TCL: -------------------------------------------------------------------------');
-    const { language } = this.props;
-    const countryField = language == 'en'? 'countryEn' : 'countryAr';
-    if(countriesOptions.length) {
-      const results = countriesOptions.filter( country => country[countryField].includes(query) );
-      this.setState({ countriesSuggestions: results });
-    }
-  }
-
-  onAutocomplete = ({ value }) => {
-    console.log('TCL: ---------------------------------------------------');
-    console.log('TCL: SignUpParent -> onAutocomplete -> value', value);
-    console.log('TCL: ---------------------------------------------------');
-    const { dispatch, language } = this.props;
-    const countryField = language == 'en'? 'countryEn' : 'countryAr';
-    dispatch(change('signUpParent', countryField, value ));
-  }
-
+  // render
   render() {
-    const { trans, handleSubmit, pristine, submitting, language } = this.props;
-    const { idTypeOptions, countriesSuggestions } = this.state;
-    const countryField = language == 'en'? 'countryEn' : 'countryAr';
+    const { idTypeOptions, countriesOptions } = this.state;
+    const { trans, handleSubmit, pristine, submitting } = this.props;
     return (
-      <form className="rtl" onSubmit={handleSubmit( userActions.addParent({ method: 'post', url: '/Users/Add'}) )}>
-      {/* form title */}
-      <h4 className="orange-text">{ trans("users.signUpParent.title") }</h4>
-      <div className="divider orange" />
-      {/* fName */}
-      <Field name="fName"
-              label={trans("users.signUpParent.fields.fName")}
-              component={renderInput} />
-      {/* mName */}
-      <Field name="mName"
-              label={trans("users.signUpParent.fields.mName")}
-              component={renderInput} />
-      {/* gName */}
-      <Field name="gName"
-              label={trans("users.signUpParent.fields.gName")}
-              component={renderInput} />
-      {/* lName */}
-      <Field name="lName"
-              label={trans("users.signUpParent.fields.lName")}
-              component={renderInput} />
-      {/* countries */}
-      {/* <Field name={countryField}
-              label={trans("users.signUpParent.fields.countryId")}
-              suggestions={countriesSuggestions}
-              completeMethod={this.doAutoComplete}
-              onChange={this.onAutocomplete}
-              options={ { field: countryField, dropdown: true } }
-              component={renderAutoComplete} /> */}
-      {/* <Field name="countryId"
-              type="autocomplete"
-              label={trans("users.signUpParent.fields.countryId")}
-              component={renderInput} /> */}
-      {/* idType */}
-      <Field name="idType"
-              type="radio"
-              options={idTypeOptions}
-              labelClassName="active"
-              itemsClassName="validate check-options"
-              label={trans("users.signUpParent.fields.idType")}
-              component={renderCheck} />
-      {/* parentId */}
-      <Field name="parentId"
-              label={trans("users.signUpParent.fields.parentId")}
-              component={renderInput} />
-      {/* idExpireDate */}
-      <Field name="idExpireDate"
-              label={trans("users.signUpParent.fields.idExpireDate")}
-              HLabel={trans("users.signUpParent.fields.idExpireDateH")}
-              GLabel={trans("users.signUpParent.fields.idExpireDateG")}
-              HKey="idExpireDateH"
-              GKey="idExpireDateG"
-              onSelect={this.setDates}
-              component={renderHGDatepicker} />
-      {/* mobile */}
-      <Field name="mobile"
-              label={trans("users.signUpParent.fields.mobile")}
-              component={renderInput} />
-      {/* email */}
-      <Field name="email"
-              label={trans("users.signUpParent.fields.email")}
-              component={renderInput} />
-      {/* userId */}
-      <Field name="userId"
-              label={trans("users.signUpParent.fields.userId")}
-              component={renderInput} />
-      {/* password */}
-      <Field name="password"
-              type="password"
-              icon="vpn_key"
-              label={trans("users.signUpParent.fields.password")}
-              component={renderInput} />
-      {/* Action Button */}
-      <Button classes="primary darken-3"
-                name="signUpParentBtn"
-                icon="send"
-                label={ trans("users.signUpParent.fields.btn") }
-                disabled={pristine || submitting}
-      />
-    </form>
+      <form className="rtl" onSubmit={handleSubmit(userActions.addParent({ method: 'post', url: '/Users/Add' }))}>
+        {/* form title */}
+        <h4 className="orange-text">{trans("users.signUpParent.title")}</h4>
+        <div className="divider orange" />
+        {/* fName */}
+        <Field name="fName"
+          label={trans("users.signUpParent.fields.fName")}
+          component={renderInput} />
+        {/* mName */}
+        <Field name="mName"
+          label={trans("users.signUpParent.fields.mName")}
+          component={renderInput} />
+        {/* gName */}
+        <Field name="gName"
+          label={trans("users.signUpParent.fields.gName")}
+          component={renderInput} />
+        {/* lName */}
+        <Field name="lName"
+          label={trans("users.signUpParent.fields.lName")}
+          component={renderInput} />
+        {/* countries */}
+        <Field name="countryId"
+          formName={formName}
+          label={trans("users.signUpParent.fields.countryId")}
+          className="validate autocompelete-select"
+          settings={{ isClearable: true }}
+          options={countriesOptions}
+          component={renderAutoComplete} />
+        {/* idType */}
+        <Field formName={formName}
+          name="idType"
+          type="radio"
+          options={idTypeOptions}
+          labelClassName="active"
+          itemsClassName="validate check-options"
+          inputClassName="with-gap"
+          label={trans("users.signUpParent.fields.idType")}
+          component={renderCheck} />
+        {/* parentId */}
+        <Field name="parentId"
+          label={trans("users.signUpParent.fields.parentId")}
+          component={renderInput} />
+        {/* idExpireDate */}
+        <Field name="idExpireDate"
+          label={trans("users.signUpParent.fields.idExpireDate")}
+          HLabel={trans("users.signUpParent.fields.idExpireDateH")}
+          GLabel={trans("users.signUpParent.fields.idExpireDateG")}
+          HKey="idExpireDateH"
+          GKey="idExpireDateG"
+          onSelect={this.setDates}
+          component={renderHGDatepicker} />
+        {/* mobile */}
+        <Field name="mobile"
+          label={trans("users.signUpParent.fields.mobile")}
+          component={renderInput} />
+        {/* email */}
+        <Field name="email"
+          label={trans("users.signUpParent.fields.email")}
+          component={renderInput} />
+        {/* userId */}
+        <Field name="userId"
+          label={trans("users.signUpParent.fields.userId")}
+          component={renderInput} />
+        {/* password */}
+        <Field name="password"
+          type="password"
+          icon="vpn_key"
+          label={trans("users.signUpParent.fields.password")}
+          component={renderInput} />
+        {/* Action Button */}
+        <Button classes="primary darken-3"
+          name="signUpParentBtn"
+          icon="send"
+          label={trans("users.signUpParent.fields.btn")}
+          disabled={pristine || submitting}
+        />
+      </form>
     )
   }
-
-  componentDidUpdate(prevState) {
-    const { countriesOptions } = this.state;
-    const { dispatch } = this.props;
-    if(this.initAutoComplete) {
-      // init the autocomplete materialize control
-      initAutoComplete({
-        'countryId': {
-          data: countriesOptions,
-          onAutocomplete: (selectedCountry) => {
-            const selectedCountryId = this.state.countriesOptions[selectedCountry];
-            dispatch(change('signUpParent', 'countryId', selectedCountryId ));
-          }
-        }
-      });
-      this.initAutoComplete = false;
-    }
-  }
-
 }
+// #endregion
 
+// #region The validate function
 const validate = () => {
 
 }
+// #endregion
 
-// define the redux form
+// #region define the redux form
 const _SignUpParent = reduxForm({
-  form: 'signUpParent',
+  form: formName,
   enableReinitialize: true,
   validate
 })(SignUpParent);
+// #endregion
 
+// #region map Redux State To Props
 const mapStateToProps = state => ({
   "docTypes": state.lookup.docTypes,
   "countries": state.lookup.countries,
+  "initialValues": initialValues,
   "trans": getTranslate(state.localize),
   "language": getActiveLanguage(state.localize).code
 });
+// #endregion
 
+// exported component
 export default connect(mapStateToProps)(_SignUpParent);
