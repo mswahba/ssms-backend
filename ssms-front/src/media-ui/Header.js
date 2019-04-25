f036c93739127e709bd6d5791f0189b558116b8b
ahmady016 <ahmady09@gmail.com>
2019-04-25 08:05:08 +0200

    add translation in media-ui Header nav

--- ssms-front/src/media-ui/Header.js
+++ ssms-front/src/media-ui/Header.js
@@ -1,68 +1,78 @@
 import React from 'react'
 import { Link, NavLink } from 'react-router-dom'
+import { connect } from 'react-redux'
+import { getTranslate } from 'react-localize-redux'
 import { initSidenav, initDropdown } from '../helpers'
 
-export default function Header({}) {
+function Header({ trans }) {
+  // after component mounted
   React.useEffect(() => {
     initSidenav();
     initDropdown();
   }, []);
+  // render
   return (
     <div className="rtl">
       <div className="navbar-fixed">
         <nav>
           <div className="nav-wrapper">
-            <Link to="/home" className="brand-logo left">Logo</Link>
+            <Link to="/home" className="brand-logo">{trans("home.title")}</Link>
             <Link to="#!" data-target="mobile-nav" className="sidenav-trigger hide-on-large-only">
               <i className="material-icons">menu</i>
             </Link>
-            <ul id="nav-mobile" className="hide-on-med-and-down">
-              <li><NavLink to="/home">Home</NavLink></li>
+            <ul id="main-nav" className="hide-on-med-and-down left">
+              <li><NavLink to="/home">{trans("home.nav.home")}</NavLink></li>
               <li>
-                <a class="dropdown-trigger" data-target="about-dropdown">
-                  About
-                  <i class="material-icons right">arrow_drop_down</i>
+                <a className="dropdown-trigger" data-target="about-dropdown">
+                  <i className="material-icons left">arrow_drop_down</i>
+                  {trans("home.nav.about")}
                 </a>
               </li>
-              <li><NavLink to="/sections">Sections</NavLink></li>
-              <li><NavLink to="/facilities">Facilities</NavLink></li>
-              <li><NavLink to="/admission">Admission</NavLink></li>
+              <li><NavLink to="/sections">{trans("home.nav.sections")}</NavLink></li>
+              <li><NavLink to="/facilities">{trans("home.nav.facilities")}</NavLink></li>
+              <li><NavLink to="/admission">{trans("home.nav.admission")}</NavLink></li>
             </ul>
           </div>
         </nav>
       </div>
-      <ul class="sidenav" id="mobile-nav">
+      <ul id="mobile-nav" className="sidenav">
         <li><NavLink to="/home">Home</NavLink></li>
         <li>
-          <a class="dropdown-trigger" data-target="about-dropdown1">
-            About
-            <i class="material-icons right">arrow_drop_down</i>
+          <a className="dropdown-trigger" data-target="about-dropdown1">
+            <i className="material-icons left">arrow_drop_down</i>
+            {trans("home.nav.about")}
           </a>
         </li>
         <li><NavLink to="/sections">Sections</NavLink></li>
         <li><NavLink to="/facilities">Facilities</NavLink></li>
         <li><NavLink to="/admission">Admission</NavLink></li>
       </ul>
-      <ul id="about-dropdown" class="dropdown-content">
-        <li><a href="#!">Vision & Mission</a></li>
-        <li class="divider"></li>
-        <li><a href="#!">General Supervisor's Message</a></li>
-        <li class="divider"></li>
-        <li><a href="#!">Our School's History</a></li>
-        <li class="divider"></li>
-        <li><a href="#!">Organizational Structure</a></li>
-        <li class="divider"></li>
+      <ul id="about-dropdown" className="dropdown-content">
+        <li><NavLink to="#!">{trans("home.nav.visionMission")}</NavLink></li>
+        <li className="divider"></li>
+        <li><NavLink to="#!">{trans("home.nav.supervisorMessage")}</NavLink></li>
+        <li className="divider"></li>
+        <li><NavLink to="#!">{trans("home.nav.schoolHistory")}</NavLink></li>
+        <li className="divider"></li>
+        <li><NavLink to="#!">{trans("home.nav.organizationalStructure")}</NavLink></li>
+        <li className="divider"></li>
       </ul>
-      <ul id="about-dropdown1" class="dropdown-content">
-        <li><a href="#!">Vision & Mission</a></li>
-        <li class="divider"></li>
-        <li><a href="#!">General Supervisor's Message</a></li>
-        <li class="divider"></li>
-        <li><a href="#!">Our School's History</a></li>
-        <li class="divider"></li>
-        <li><a href="#!">Organizational Structure</a></li>
-        <li class="divider"></li>
-      </ul>      
+      <ul id="about-dropdown1" className="dropdown-content">
+        <li><NavLink to="#!">{trans("home.nav.visionMission")}</NavLink></li>
+        <li className="divider"></li>
+        <li><NavLink to="#!">{trans("home.nav.supervisorMessage")}</NavLink></li>
+        <li className="divider"></li>
+        <li><NavLink to="#!">{trans("home.nav.schoolHistory")}</NavLink></li>
+        <li className="divider"></li>
+        <li><NavLink to="#!">{trans("home.nav.organizationalStructure")}</NavLink></li>
+      </ul>
     </div>
   )
 }
+// select the values needed form redux state
+// get the translate function from localize
+const mapStateToProps = (state) => ({
+  "trans": getTranslate(state.localize)
+})
+// connect the form with the redux state
+export default connect(mapStateToProps)(Header);
