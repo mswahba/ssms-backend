5a30723145aac2c793a8728fc55352e3bbc70ad7
ahmady016 <ahmady09@gmail.com>
2019-04-26 11:30:03 +0200

    do Sidenav edge based on lang, set activeNavLink

--- ssms-front/src/shared/Navbar.js
+++ ssms-front/src/shared/Navbar.js
@@ -2,25 +2,27 @@ import React from 'react'
 import { Link, NavLink } from 'react-router-dom'
 import { initSidenav, initDropdown } from '../helpers'
 
-const renderNavLinks = (id, className, navLinks) => (
-  <ul id={id} className={className}>
-    {navLinks.map((link, i) => (
-      <li key={i + 1}>
-        {(link.path)
-          ? <NavLink to={link.path}>
-              <i className={`${link.icon} right`}></i>
-              {link.text}
-            </NavLink>
-          : <a data-target={ (id === 'main-nav')? link.id+'-main' : link.id+'-mobile' } className="dropdown-trigger">
-              <i className={`${link.icon} right`}></i>
-              {link.text}
-              <i className="material-icons left">arrow_drop_down</i>
-            </a>
-        }
-      </li>
-    ))}
-  </ul>
-)
+const renderNavLinks = (id, className, navLinks, activeLink, setActiveLink) => {
+  return (
+    <ul id={id} className={className}>
+      {navLinks.map((link, i) => (
+        <li key={i + 1} className={activeLink.includes(link.path) ? 'active' : ''}>
+          {(link.path)
+            ? <NavLink to={link.path} onClick={() => setActiveLink(link.path)} >
+                <i className={`${link.icon} right`}></i>
+                {link.text}
+              </NavLink>
+            : <a data-target={ (id === 'main-nav')? link.id+'-main' : link.id+'-mobile' } className="dropdown-trigger">
+                <i className={`${link.icon} right`}></i>
+                {link.text}
+                <i className="material-icons left">arrow_drop_down</i>
+              </a>
+          }
+        </li>
+      ))}
+    </ul>
+  )
+}
 
 const renderNavSubLinks = (navLinks) => {
   const linksWithChildren = navLinks.filter( link => link.children && Array.isArray(link.children) )
@@ -30,7 +32,7 @@ const renderNavSubLinks = (navLinks) => {
         {link.children.map((item, i) => (
           <li key={i + 1}>
             <NavLink to={item.path}>
-              <i className="fas fa-hand-point-left right"></i>
+              <i className={`${item.icon || 'fas fa-file-alt'} right`}></i>
               {item.text}
             </NavLink>
           </li>
@@ -40,7 +42,7 @@ const renderNavSubLinks = (navLinks) => {
         {link.children.map((item, i) => (
           <li key={i + 1}>
             <NavLink to={item.path}>
-              <i className="fas fa-hand-point-left right"></i>
+              <i className={`${item.icon || 'fas fa-file-alt'} right`}></i>
               {item.text}
             </NavLink>
           </li>
@@ -50,10 +52,11 @@ const renderNavSubLinks = (navLinks) => {
   ))
 }
 
-export default function Navbar({ navTitle, navLinks }) {
+export default function Navbar({ navTitle, navLinks, lang }) {
+  const [activeLink, setActiveLink] = React.useState(window.location.pathname)
   // after component mounted
   React.useEffect(() => {
-    initSidenav();
+    initSidenav({ edge: (lang === 'ar')? 'right' : 'left' });
     initDropdown();
   }, []);
   // render
@@ -64,11 +67,11 @@ export default function Navbar({ navTitle, navLinks }) {
           <div className="nav-wrapper">
             <Link to={navTitle.path} className="brand-logo">{navTitle.text}</Link>
             <a data-target="mobile-nav" className="sidenav-trigger hide-on-large-only"><i className="material-icons">menu</i></a>
-            {renderNavLinks("main-nav", "hide-on-med-and-down left", navLinks)}
+            {renderNavLinks("main-nav", "hide-on-med-and-down left", navLinks, activeLink, setActiveLink)}
           </div>
         </nav>
       </div>
-      {renderNavLinks("mobile-nav", "sidenav", navLinks)}
+      {renderNavLinks("mobile-nav", "sidenav", navLinks, activeLink, setActiveLink)}
       {renderNavSubLinks(navLinks)}
     </>
   )
