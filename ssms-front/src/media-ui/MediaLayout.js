15e901ac513de74cd634f90bf27c2125bd0e6266
ahmady016 <ahmady09@gmail.com>
2019-07-11 10:39:46 +0200

    move media routes to its file

--- ssms-front/src/media-ui/MediaLayout.js
+++ ssms-front/src/media-ui/MediaLayout.js
@@ -1,48 +1,27 @@
 import React from 'react'
 import { connect } from 'react-redux'
 import { getTranslate, getActiveLanguage } from 'react-localize-redux'
+
+import { navLinks, routes } from './routes'
+
 import Header from './Header'
 import MediaRoutes from './MediaRoutes'
-import Home from './home/Home'
-import Sections from './Sections'
-import Facilities from './Facilities'
-import Admission from './Admission'
-import VisionMission from './about/VisionMission'
-import SupervisorMessage from './about/SupervisorMessage'
-import SchoolHistory from './about/SchoolHistory'
-import OrganizationalStructure from './about/OrganizationalStructure'
 import Footer from './Footer'
 import './media-ui.css'
 
 function MediaLayout ({ trans, lang }) {
   const defaultPath = '/home'
   const navTitle = { path: '/home', text: trans("home.title") }
-  const navLinks = [
-    { path: '/home', text: trans('home.nav.home'), icon: 'fas fa-home', component: Home },
-    { path: '/sections', text: trans('home.nav.sections'), icon: 'fas fa-landmark', component: Sections },
-    { path: '/facilities', text: trans('home.nav.facilities'), icon: 'fas fa-flask', component: Facilities },
-    { path: '/admission', text: trans('home.nav.admission'), icon: 'fas fa-check-circle', component: Admission },
-    {
-      id: 'about-dropdown',
-      icon: 'fas fa-info-circle',
-      text: trans('home.nav.about'),
-      path: '/about',
-      children: [
-        { path: '/about/vision-mission', text: trans("home.nav.visionMission"), component: VisionMission },
-        { path: '/about/supervisor-message', text: trans("home.nav.supervisorMessage"), component: SupervisorMessage },
-        { path: '/about/school-history', text: trans("home.nav.schoolHistory"), component: SchoolHistory },
-        { path: '/about/organizational-structure', text: trans("home.nav.organizationalStructure"), component: OrganizationalStructure },
-      ]
-    },
-  ]
+
   return (
     <div className='rtl'>
-      <Header navTitle={navTitle} navLinks={navLinks} lang={lang} defaultPath={defaultPath} />
-      <MediaRoutes navLinks={navLinks} defaultPath={defaultPath} />
+      <Header navTitle={navTitle} navLinks={navLinks(trans)} lang={lang} defaultPath={defaultPath} />
+      <MediaRoutes navLinks={routes(trans)} defaultPath={defaultPath} />
       <Footer />
     </div>
   )
 }
+
 // select the values needed form redux state [get the translate function from localize]
 const mapStateToProps = (state) => ({
   "trans": getTranslate(state.localize),
---
+++ ssms-front/src/media-ui/routes.js
@@ -0,0 +1,36 @@
+import Home from './home/Home'
+import Sections from './Sections'
+import Facilities from './Facilities'
+import Admission from './Admission'
+import VisionMission from './about/VisionMission'
+import SupervisorMessage from './about/SupervisorMessage'
+import SchoolHistory from './about/SchoolHistory'
+import OrganizationalStructure from './about/OrganizationalStructure'
+
+export const navLinks = (trans) => ([
+  { path: '/home', text: trans('home.nav.home'), icon: 'fas fa-home', component: Home },
+  { path: '/sections', text: trans('home.nav.sections'), icon: 'fas fa-landmark', component: Sections },
+  { path: '/facilities', text: trans('home.nav.facilities'), icon: 'fas fa-flask', component: Facilities },
+  { path: '/admission', text: trans('home.nav.admission'), icon: 'fas fa-check-circle', component: Admission },
+  {
+    id: 'about-dropdown',
+    icon: 'fas fa-info-circle',
+    text: trans('home.nav.about'),
+    path: '/about',
+    children: [
+      { path: '/about/vision-mission', text: trans("home.nav.visionMission"), component: VisionMission },
+      { path: '/about/supervisor-message', text: trans("home.nav.supervisorMessage"), component: SupervisorMessage },
+      { path: '/about/school-history', text: trans("home.nav.schoolHistory"), component: SchoolHistory },
+      { path: '/about/organizational-structure', text: trans("home.nav.organizationalStructure"), component: OrganizationalStructure },
+    ]
+  },
+])
+
+export const links = (trans) => ([
+
+])
+
+export const routes = (trans) => ([
+  ...navLinks(trans),
+  ...links(trans)
+])
